eb7d8cfde7547c8eb284273f51e1b1c1a92a6867
digitrom <roma.bobrovski@gmail.com>
2023-02-08 18:02:43 +0300

    .Filter - End of lesson 4

--- src/04/04_02.ts
+++ src/04/04_02.ts
@@ -4,6 +4,6 @@ export const demolishHouseOnTheStreet = (city:CityType, streetTitle:string) => {
     city.houses = city.houses.filter(house => house.address.street.title !== streetTitle)
 }
 
-export const getBuildingsWithStaffCountGreaterThan = (buildings: Array<GovBuildingType>, number: number) => {
-return buildings.filter(building => building.staffCount > number)
+export const getBuildingsWithStaffCountGreaterThan = (govBuildings: Array<GovBuildingType>, number: number) => {
+return govBuildings.filter(building => building.staffCount > number)
 }
