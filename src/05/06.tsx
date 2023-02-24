df3d01b65d31b69fefd08be8263b1433d5e24e78
digitrom <roma.bobrovski@gmail.com>
2023-02-24 13:34:49 +0300

    6 - onClick done

--- src/05/06.tsx
+++ src/05/06.tsx
@@ -1,3 +1,5 @@
+import React, {ChangeEvent, MouseEvent} from "react";
+
 // const callback = (): number => {
 //     alert('hey')
 //     return 12;
@@ -6,17 +8,40 @@
 // window.setTimeout(callback, 1000)
 
 export const User = () => {
-    const deleteUser = () => {
-        alert('user have been deleted');
+    const deleteUser = (e: MouseEvent<HTMLButtonElement>) => {
+        alert(e.currentTarget.name);
     }
 
     const saveUser = () => {
         alert('user have been saved');
     }
+    const onNameChanged = () => {
+        console.log("name changed")
+    }
+    const onAgeChanged = (e: ChangeEvent<HTMLInputElement>) => {
+        console.log("age changed " + e.currentTarget.value)
+    }
+
+    const focusLostHandler = () => {
+        console.log("focus lost")
+    }
+
+    return <div><textarea onBlur={focusLostHandler} onChange={onNameChanged}>Roma</textarea>
+        <input type={"number"} onChange={onAgeChanged} onBlur={focusLostHandler}/>
+
+        {/*<button*/}
+        {/*    name={"delete"}*/}
+        {/*    tabIndex={0}*/}
+        {/*    onBlur={focusLostHandler}*/}
+        {/*    onClick={deleteUser}>delete*/}
+        {/*</button>*/}
+        <button
+            name={"save"}
+            onClick={saveUser}>save</button>
 
 
-    return <div>Roma
-        <button onClick={deleteUser}>delete</button>
-        <button onClick={saveUser}>save</button>
+        {/*<div tabIndex={1} onClick={deleteUser}>delete</div>*/}
+        {/*<div tabIndex={0} onClick={saveUser}>save</div>*/}
+        {/* tabIndex через клавишу TAB меняет фокус и переключаеся между дивками*/}
     </div>
 }
